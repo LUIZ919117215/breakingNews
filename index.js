1418b90c44922d6590a7bb20bb01df8f4f62a8cc
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-04-23 08:15:23 -0300

    refactor: added the soft archutecture

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const express = require('express')
+const userRoute = require("./src/routes/user.route")
 const app = express()
 
- //ROTA
-   // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-   // GET - Pega uma info
-   // POST - Cria uma info
-   // PUT - Altera toda a info
-   // PATH - Altera parte da info
-   // DELETE - Apaga uma info
-   
- //Name - Um identificador da rota
-
- //Function (Callback) - Responsavel por executar algum comando
-app.get('/soma',(req, res) => {
-    const soma = 1 + 1;
-
-  res.json({soma:soma})
-})
+app.use("/soma", userRoute)
 
 app.listen(3000)
