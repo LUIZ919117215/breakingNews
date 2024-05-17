a602b8fafb54eeb62543f303fb1b43870510fc0a
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-05-17 10:15:30 -0300

    aula 14: refactor: add bcrypt on password

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -1,8 +1,7 @@
-const mongoose = require("mongoose")
-const userService = require('../services/user.service')
+import mongoose from 'mongoose';
+import userService from '../services/user.service.js'
 
-
-const validId = (req, res, next) => {
+export const validId = (req, res, next) => {
     try {
         const id = req.params.id
 
@@ -17,7 +16,7 @@ const validId = (req, res, next) => {
     }
 }
 
-const validUser = async (req, res, next) => {
+export const validUser = async (req, res, next) => {
     try {
         const id = req.params.id
 
@@ -35,9 +34,4 @@ const validUser = async (req, res, next) => {
     } catch (err) {
         res.status(500).send({ message: err.message })
     }
-}
-
-module.exports = { 
-    validId, 
-    validUser 
 }
