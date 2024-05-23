71c50a3782992b8f839431c16b7d8bf100e2d3d3
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-05-23 16:09:33 -0300

    aula 20: refactor: add the authentication middleware

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -2,8 +2,9 @@ import { Router } from "express"
 const router = Router()
 
 import { create, findAll } from "../controllers/news.controller.js"
+import { authmiddleware } from '../middlewares/auth.middlewaers.js';
 
-router.post("/", create)
+router.post("/", authmiddleware, create)
 router.get("/", findAll)
 
 export default router
