d640594bbe8e0d074e66b7e25e97d0677ccb8a06
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-05-25 12:29:55 -0300

    aula 25: refactor: add search by user to  API

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -4,11 +4,15 @@
 import { Router } from "express"
 const router = Router()
 
-import { create, findAll, topNews } from "../controllers/news.controller.js"
+import { create, findAll, topNews, findById, searchByTitle, byUser } from "../controllers/news.controller.js"
 import { authmiddleware } from '../middlewares/auth.middlewaers.js'
 
 router.post("/", authmiddleware, create)
 router.get("/", findAll)
 router.get("/top", topNews)
+router.get("/search", searchByTitle)
+router.get("/byUser", authmiddleware, byUser)
+
+router.get("/:id", authmiddleware, findById)
 
 export default router
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,17 +3,16 @@
 //código entendido
 import News from '../models/News.js';
 
-const createService = (body) => News.create(body)
+export const createService = (body) => News.create(body)
 
-const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate('user')    
+export const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate('user')    
 
-const countNews = () => News.countDocuments()
+export const countNews = () => News.countDocuments()
 
-const topNewsService = () => News.findOne().sort({_id: -1}).populate('user')
+export const topNewsService = () => News.findOne().sort({_id: -1}).populate('user')
 
-export {
-    createService,
-    findAllService,
-    countNews,
-    topNewsService
-}
+export const findByIdService = (id) => News.findById(id).populate('user')
+
+export const searchByTitleService = (title) => News.find({ title: { $regex: `${title || ""}`, $options: "i" } }).sort({_id: -1}).populate('user')
+
+export const byUserService = (id) => News.find({user: id}).sort({_id: -1}).populate('user')
