8ee7a379bbc5cd1b1aba6a10ad5a4bf2be8f33fc
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-05-30 23:15:16 -0300

    change start to index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 //tentando
 //código entendido//
 import express from 'express'
-import connectDatabase from './src/database/db.js'
+import connectDatabase from './database/db.js'
 import dotenv from "dotenv"
 
 
-import  userRoute from './src/routes/user.route.js'
-import  authRoute from './src/routes/auth.route.js'
-import  newsRoute from './src/routes/news.route.js'
-import  swaggerRoute from './src/routes/swagger.route.js'
+import  userRoute from './routes/user.route.js'
+import  authRoute from './routes/auth.route.js'
+import  newsRoute from './routes/news.route.js'
+import  swaggerRoute from './routes/swagger.route.js'
 
 dotenv.config()
 
