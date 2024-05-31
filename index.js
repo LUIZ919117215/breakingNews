90cf30a33e71fef1b3cc183f5461df5a527a6bfa
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-05-30 23:51:15 -0300

    NEW CHANGES

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 //tentando
 //código entendido//
 import express from 'express'
-import connectDatabase from './database/db.js'
+import connectDatabase from './src/database/db.js'
 import dotenv from "dotenv"
 
 
-import  userRoute from './routes/user.route.js'
-import  authRoute from './routes/auth.route.js'
-import  newsRoute from './routes/news.route.js'
-import  swaggerRoute from './routes/swagger.route.js'
+import  userRoute from './src/routes/user.route.js'
+import  authRoute from './src/routes/auth.route.js'
+import  newsRoute from './src/routes/news.route.js'
+import  swaggerRoute from './src/routes/swagger.route.js'
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT_URL || 3000
 const app = express()
 
 connectDatabase()
