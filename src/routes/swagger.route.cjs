eaf57966b117c534befdabe8df0cea2e832ce571
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-06-08 21:56:46 -0300

    bbb

--- src/routes/swagger.route.cjs
+++ src/routes/swagger.route.cjs
@@ -1,10 +1,9 @@
-import { Router } from 'express';
-const router = Router();
+const router = require("express").Router();
 
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from '../swagger.json' assert { type: "json"}
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("../swagger.json")
 
 router.use("/", swaggerUi.serve)
 router.get("/", swaggerUi.setup(swaggerDocument))
 
-export default router
+module.exports = router
