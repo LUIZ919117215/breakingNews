b24f1a8df40a3e8c0847ce0a5b1c5a71bac8767f
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-06-19 08:53:21 -0300

    add cors lib

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ import  userRoute from './src/routes/user.route.js'
 import  authRoute from './src/routes/auth.route.js'
 import  newsRoute from './src/routes/news.route.js'
 import  swaggerRoute from './src/routes/swagger.route.cjs'
+import cors from 'cors'
+
 
 dotenv.config()
 
@@ -19,6 +21,8 @@ const port = process.env.PORT_URL
 const app = express()
 
 connectDatabase()
+
+app.use(cors())
 app.use(express.json())
 app.use("/user", userRoute)
 app.use("/auth", authRoute)
