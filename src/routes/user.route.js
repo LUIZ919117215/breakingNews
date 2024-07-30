0c658e6807b722bf618885879cec049385cd8ac3
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-07-29 23:50:16 -0300

    something wrong

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -13,7 +13,7 @@ router.post("/create", userController.create)//testada!!
 router.use(authmiddleware)
 router.get("/", userController.findAll)//testada!!
 
-//router.use(validId)
+//router.use(validId) //conserte o middleware vslidId e despois, o descomente!!
 router.get("/findById/:id?", userController.findById)//testada!!
 router.patch("/:id",validId, validUser, userController.update)//testada!!
 
